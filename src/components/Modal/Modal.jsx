39849b40c27f8a29c4595c9418fbffaa9bc95600
Wertoquri <kirilpojduk@gmail.com>
2026-02-15 15:45:05 +0200

    add validation

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,11 +3,20 @@ import style from "./Modal.module.scss"
 import { IoCloseSharp } from "react-icons/io5";
 import { useContext } from 'react'
 import { ContextStore } from '../../store/ContextStore.jsx';
+import { useForm } from "react-hook-form";
 
 
 export default function Modal(props) {
     let { addEvent } = useContext(ContextStore)
 
+    let { register, handleSubmit, formState: { errors }, reset } = useForm()
+
+    const submit = (data) => {
+        console.log(data)
+        addEvent(data)
+        props.open(false)
+        reset()
+    }
 
     return (
         <div className={style.wrapper} onClick={(e) => {
@@ -15,30 +24,66 @@ export default function Modal(props) {
                 props.open(false)
             }
         }}>
-            <div className={style.inner}>
+            <form onSubmit={handleSubmit(submit)} className={style.inner}>
                 <button className={style.closeButton} onClick={() => props.open(false)}><IoCloseSharp /></button>
                 <h1>Додати подію</h1>
                 <section>
                     <label htmlFor="title"> Назва події</label>
-                    <input type="text" name='title' id="title" />
+                    <input type="text" name='title' id="title" {...register("title", {
+                        required: true,
+                        minLength: 3,
+                        maxLength: 40,
+                        pattern: {
+                            value: /^[a-zA-Z0-9\s]*$/,
+                            message: "Only letters, numbers and spaces are allowed"
+                        },
+                        message: "Invalid title"
+                    })} />
 
                 </section>
                 <section>
                     <label htmlFor="date"> Дата події</label>
-                    <input type="date" name='date' id="date" />
+                    <input type="date" name='date' id="date" {...register("date"
+                        , {
+                            required: {
+                                value: true,
+                                message: "Date is required"
+                            },
+
+                        }
+                    )} />
 
                 </section>
                 <section>
                     <label htmlFor="time"> Час події</label>
-                    <input type="time" name='time' id="time" />
+                    <input type="time" name='time' id="time" {...register("time", {
+                        required: {
+                            value: true,
+                            message: "Time is required"
+                        }
+
+                    })} />
 
                 </section>
                 <section>
                     <label htmlFor="color"> Колір події</label>
-                    <input type="color" name='color' id="color" />
+                    <input type="color" name='color' id="color" {...register("color"
+                        , {
+                            value: "#000000"
+                        }
+                    )
+
+                    } />
                 </section>
-                <button>Додати подію</button>
-            </div>
+                <section>
+                    {(errors.title || errors.date || errors.time) && (<span className={style.error}>
+                        {errors.title?.message}
+                        {errors.date?.message}
+                        {errors.time?.message}
+                    </span>)}
+                </section>
+                <button type="submit" >Додати подію</button>
+            </form>
 
         </div>
     )
