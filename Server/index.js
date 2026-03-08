2bc761a0170c5446510b4b4b9f7bf9a1f0ea84a5
Wertoquri <kirilpojduk@gmail.com>
2026-03-08 15:40:28 +0200

    add hooks, login, register

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import db from './db.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 app.use(express.json());
@@ -22,8 +23,51 @@ app.post('/register', async (req, res) => {
         await db.query("INSERT INTO user (login, password, email) VALUES (?, ?, ?)", [login, hashedPassword, email]);
         res.status(201).json({message: 'User registered successfully'});
     }catch(err){
+        res.status(500).json({message: err});
+    }
+});
+
+app.post("/login", async (req, res) => {
+    const {email, password} = req.body;
+    try{
+        let [users] = await db.query("SELECT * FROM user WHERE email = ?", [email]);
+        if(users.length === 0){
+            res.status(401).json({message: "invalid email or password"});
+            return;
+        }
+        let user = users[0];
+        let isValidPassword = await bcrypt.compare(password, user.password);
+        if(!isValidPassword){
+            res.status(401).json({message: "invalid email or password"});
+            return;
+        }
+
+        let token = jwt.sign({id: user.id}, process.env.SECRET, {expiresIn: '14d'})
+        res.status(200).json({token});
+    }catch(err){
         res.status(500).json({message: 'Server error'});
     }
+
+});
+
+const authenticateToken = (req, res, next) => {
+    let authHeader = req.headers.authorization;
+    let token = authHeader && authHeader.split(' ')[1];
+    if(!token){
+        return res.status(403).json({error: "No token provided"});
+    }
+    jwt.verify(token, process.env.SECRET, (err, user) => {
+        if(err){
+            return res.status(403).json({error: "Invalid token"});
+        }
+        req.user = user;
+        next();
+    });
+    
+}
+
+app.get("/protected", authenticateToken, (req, res) => {
+    res.json({data: req?.user?.id});
 });
 
 app.listen(3000,()=>console.log('Server is running on port 3000'));
